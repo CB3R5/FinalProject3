4e941e12dd81628b9221bb805f4380aeb9de6c36
Leonel Cabral <69341023+CB3R5@users.noreply.github.com>
2025-06-30 05:40:28 -0300

    Update TokenA.sol

--- TokenA.sol
+++ TokenA.sol
@@ -3,15 +3,13 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenA is ERC20, Ownable {
-    constructor() ERC20("TokenA", "TKA") Ownable(msg.sender) {
-        uint256 initialSupply = 100_000_000_000 * 10 ** decimals();
-        _mint(msg.sender, initialSupply);
-    }
+contract MyTokens is ERC20 {
+    constructor(string memory sym, string memory name)
+        ERC20(name, sym)
+    {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
