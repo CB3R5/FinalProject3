071dc8ea904d33b794be5bc67931376b4c8b5fd4
Leonel Cabral <69341023+CB3R5@users.noreply.github.com>
2025-07-02 01:57:16 -0300

    Update tokenb.sol

--- tokenb.sol
+++ tokenb.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title TokenB
+/// @notice ERC20 token used as tokenB in the SimpleSwap contract
+contract TokenB is ERC20 {
+    constructor() ERC20("Token B", "USDC") {}
+
+    /// @notice Allows you to mine tokens to an account
+    /// @param to Receiving address
+    /// @param amount Amount to be minted
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
 
