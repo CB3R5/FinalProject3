6989af52ccf4ee5f3beb28088de25fc97a62acb1
Leonel Cabral <69341023+CB3R5@users.noreply.github.com>
2025-07-02 02:04:20 -0300

    Update tokenA.sol

--- tokenA.sol
+++ tokenA.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+contract TokenA is ERC20, Ownable {
+    constructor() ERC20("TokenA", "TKA") Ownable(msg.sender) {
+        uint256 initialSupply = 100_000_000_000 * 10 ** decimals();
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
